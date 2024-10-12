ea25ed09767e0e5364dc5542e696994a90754d87
lucas <lucas.cardo123@hotmail.com>
2024-10-12 11:06:35 -0400

    delete sql

--- create.sql
+++
@@ -1,6 +0,0 @@
-create table view (created_at bigint not null, comic_id varchar(255) not null, id varchar(255) not null, page_id varchar(255) not null, primary key (id));
-create table view (created_at bigint not null, comic_id varchar(255) not null, id varchar(255) not null, page_id varchar(255) not null, primary key (id));
-create table view (created_at bigint not null, comic_id varchar(255) not null, id varchar(255) not null, page_id varchar(255) not null, primary key (id));
-create table view (created_at bigint not null, comic_id varchar(255) not null, id varchar(255) not null, page_id varchar(255) not null, primary key (id));
-create table view (created_at bigint not null, comic_id varchar(255) not null, id varchar(255) not null, page_id varchar(255) not null, primary key (id));
-create table view (created_at bigint not null, comic_id varchar(255) not null, id varchar(255) not null, page_id varchar(255) not null, primary key (id));
